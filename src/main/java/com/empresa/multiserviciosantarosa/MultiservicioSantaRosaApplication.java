3de4492dc9e3e3bbb6ee4492d7bebc92a0477867
bgallardov <brayan.gallardo@inetum.com>
2025-11-13 18:14:56 -0500

    Configuracion Incial

---
+++ src/main/java/com/empresa/multiserviciosantarosa/MultiservicioSantaRosaApplication.java
@@ -0,0 +1,13 @@
+package com.empresa.multiserviciosantarosa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiservicioSantaRosaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiservicioSantaRosaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/empresa/multiserviciosantarosa/MultiservicioSantaRosaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresa.multiserviciosantarosa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiservicioSantaRosaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
